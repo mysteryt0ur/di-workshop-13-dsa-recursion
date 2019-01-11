24b9553a76fb50efead08c90735b1d4b28a91643
Eldawg Jones <eleanor.jones@rs-components.com>
2019-01-11 09:43:43 +0000

    finalised analysis of countdown algorithm

--- countdownAlgo.js
+++ countdownAlgo.js
@@ -8,3 +8,18 @@ var countdown = function(value) {
  };
  
  countdown(10);
+
+// This function does what it says on the tin. Depending on what value is given, the function will count down and log each individual figure that is 1 integer smaller than the predecesing value. This continues in a list-type fashion until the number 1 is printed, then the list reaches an end. 
+
+// So within the example given, the value is 10. This means that the list that should be printed within the command line is:
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// After checking this, I can see this to be correct. 
--- factorialAlgo.js
+++ factorialAlgo.js
@@ -7,12 +7,12 @@ var factorial = function(n) {
  }
  console.log(factorial(14));
 
-// this function attempts to recieve the factorial figure of whatever 'n' is. A factorial takes into account the number itself, and then multiplies the number by itself minus 1. This continues until the last figure is multiplied by one and then you are left with your factorial value. 
+// This function attempts to recieve the factorial figure of whatever 'n' is. A factorial takes into account the number itself, and then multiplies the number by itself minus 1. This continues until the last figure is multiplied by one and then you are left with your factorial value. 
 
-//So for example, the 'n' in this instance is a 6. This means that the system will be completing the sum:
-//6 x 5 x 4 x 3 x 2 x 1
-//This should equal 720. After testing this in node, I have found this to be true. 
+// So for example, the 'n' in this instance is a 6. This means that the system will be completing the sum:
+// 6 x 5 x 4 x 3 x 2 x 1
+// This should equal 720. After testing this in node, I have found this to be true. 
 
-//I also tested this function by using 14 as a substituate for 'n.' This means the sum being taken place by the function was:
-//14 x 13 x 12 x 11 x 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1
-//This equals the sum 87178291200 and this was also shown within the log.
+// I also tested this function by using 14 as a substituate for 'n.' This means the sum being taken place by the function was:
+// 14 x 13 x 12 x 11 x 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1
+// This equals the sum 87178291200 and this was also shown within the log.
