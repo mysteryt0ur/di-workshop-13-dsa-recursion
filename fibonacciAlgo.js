4f9eb7adf74861cf2216e46d311eeae4c9adac4f
Eldawg Jones <eleanor.jones@rs-components.com>
2019-01-11 11:08:19 +0000

    finish part 1

--- fibonacciAlgo.js
+++ fibonacciAlgo.js
@@ -9,4 +9,8 @@ function fibonacci(num) {
     return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-console.log(fibonacci(10));
+console.log(fibonacci(14));
+
+// This function attempts to display to the user a number within the fibonnaci sequence that falls within the order that they specifiy. For example, the figure given in the example is a 3, meaning that the function should log the 3rd fibonacci number within the sequence.
+
+//To test this, the 3rd fibonnaci number is 2. When trialing this through the command line, 2 is shown. I also tried this using '14' as the numberv value, and the log displayed 377 just as it should do. 
